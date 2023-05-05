d0d06a8f594e622b655765388c07328d55d955a6
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-05-04 21:54:00 -0400

    styled about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,16 @@ import './About.css'
 const About = () => {
     return(
         <div className="aboutWrapper">
-            <h1>
-                About content will go here
+            <h1 className='aboutTitle'>
+                About
             </h1>
+            <article className='aboutContent'>
+                Welcome to our Au Pair blog webpage, where we strive to connect and support individuals participating in the Au Pair program worldwide. Our social platform uses location-based features to facilitate connections between Au Pairs nearby, allowing them to exchange ideas and gain new experiences.
+                We understand that participating in the Au Pair program can be both exhilarating and daunting, especially when in a new country. Therefore, our platform prioritizes safety by providing a supportive community where individuals can connect and share their experiences while navigating their new environment.
+                Our blog is a treasure trove of informative articles, tips, and resources that cover a wide range of topics related to the Au Pair program. From finding a host family to cultural exchange and language learning, we aim to help you make the most of your experience. Whether you're a first-time Au Pair or a seasoned participant, our community is open to all.
+                Besides our location-based features and safety measures, we also offer a platform for Au Pairs to share their experiences with others, similar to Instagram. Our Au Pair blog webpage enables individuals to create posts, share photos, and insights with the community. Through this feature, Au Pairs can connect with others, establish friendships, and share their unique cultural backgrounds with people from around the globe. They can offer valuable advice and support to others who may be facing similar challenges or experiences.
+                We believe in the power of community and connection. Join us today and become part of our supportive community of Au Pairs worldwide.
+            </article>
         </div>
     )
 }
