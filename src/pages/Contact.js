1fb0bb2109e94f30d071034f6ac98a0cb08ea98e
Rafaeli Rangel <rafaelirangel@icloud.com>
2023-05-07 13:01:15 -0400

    fixed aupair program page responsive

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,53 +10,45 @@ import contactImg from '../img/contact-us.jpg'
 const Contact = () => {
     return (
         <div className='contactWrapper'>
-            <img className='contactImg'  src={contactImg}></img>
+            <img className='contactImg' src={contactImg}></img>
 
-            <h1 className='contacTitle'>Need help with anything? Don't worry, contact US</h1>
+            <div className='contactContentWrapper'>
+                <h1 className='contacTitle'>Need help with anything? Don't worry, contact US</h1>
 
-            <div className='contactContent'>
-
-                <div className='rafaInfo'>
-                    <h2 className='name'> Rafaeli Rangel </h2>
-                    <div className='github'>
-                        <Github size='36' fill='black' />
-                        <Link className='githubLink' to='https://github.com/rafaelirangel' target='_blank'>Github</Link>
+                <div className='contactContent'>
+                    <div className='rafaInfo'>
+                        <h2 className='name'> Rafaeli Rangel </h2>
+                        <div className='github'>
+                            <Github size='36' fill='black' />
+                            <Link className='githubLink' to='https://github.com/rafaelirangel' target='_blank'>Github</Link>
+                        </div>
+                        <div className='linkdin'>
+                            <LinkedIn style={{ fontSize: '2rem', color: '#307fdb' }} />
+                            <Link className='linkedinLink' to='https://linkedin.com/in/rafaelirangeldeoliveira' target='_blank'>LinkedIn</Link>
+                        </div>
+                        <div className='twitter'>
+                            <Twitter theme='filled' size='36' fill='#529bec' />
+                            <Link className='twitterLink' to='https://twitter.com/rafaelirangel06' target='_blank'> Twitter </Link>
+                        </div>
                     </div>
 
-                    <div className='linkdin'>
-                        <LinkedIn style={{ fontSize: '2rem', color: '#307fdb' }} />
-                        <Link className='linkedinLink' to='https://linkedin.com/in/rafaelirangeldeoliveira' target='_blank'>LinkedIn</Link>
-                    </div>
-
-                    <div className='twitter'>
-                        <Twitter theme='filled' size='36' fill='#529bec' />
-                        <Link className='twitterLink' to='https://twitter.com/rafaelirangel06' target='_blank'> Twitter </Link>
+                    <div className='nateInfo'>
+                        <h2 className='name'> Nathen Leonard </h2>
+                        <div className='github'>
+                            <Github size='36' fill='black' />
+                            <Link className='githubLink' to='https://github.com/nateleo91' target='_blank'>Github</Link>
+                        </div>
+                        <div className='linkdin'>
+                            <LinkedIn style={{ fontSize: '2rem', color: '#307fdb' }} />
+                            <Link className='linkedinLink' to='https://www.linkedin.com/in/nathen-leonard' target='_blank'>LinkedIn</Link>
+                        </div>
+                        <div className='twitter'>
+                            <Twitter theme='filled' size='36' fill='#529bec' />
+                            <Link className='twitterLink' to='https://twitter.com/Nathen_Leonard' target='_blank'> Twitter </Link>
+                        </div>
                     </div>
                 </div>
-
-                <div className='nateInfo'>
-                    <h2 className='name'> Nathen Leonard </h2>
-                    <div className='github'>
-                        <Github size='36' fill='black' />
-                        <Link className='githubLink' to='https://github.com/nateleo91' target='_blank'>Github</Link>
-                    </div>
-
-                    <div className='linkdin'>
-                        <LinkedIn style={{ fontSize: '2rem', color: '#307fdb' }} />
-                        <Link className='linkedinLink' to='https://www.linkedin.com/in/nathen-leonard' target='_blank'>LinkedIn</Link>
-                    </div>
-
-                    <div className='twitter'>
-                        <Twitter theme='filled' size='36' fill='#529bec' />
-                        <Link className='twitterLink' to='https://twitter.com/Nathen_Leonard' target='_blank'> Twitter </Link>
-                    </div>
-
-                </div>
-
             </div>
-           
-
-
         </div>
     )
 }
